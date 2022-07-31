f27f5b8187a539dc2323b13c44e4937eca05136e
gallardo <aiki.hosogaya@gmail.com>
2022-07-31 18:43:50 +0900

    create error message test

--- product-manage-site-for-hands-on/src/app/core/components/error-messaging/error-messaging.component.spec.ts
+++ product-manage-site-for-hands-on/src/app/core/components/error-messaging/error-messaging.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ErrorMessagingService } from '../../services/error-messaging.service';
 import { ErrorMessagingComponent } from './error-messaging.component';
 
-xdescribe('ErrorMessagingComponent', () => {
+describe('ErrorMessagingComponent', () => {
   let component: ErrorMessagingComponent;
   let fixture: ComponentFixture<ErrorMessagingComponent>;
+  let service: ErrorMessagingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,9 +17,87 @@ xdescribe('ErrorMessagingComponent', () => {
     fixture = TestBed.createComponent(ErrorMessagingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.inject(ErrorMessagingService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('#constractor', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('#getMessageProperty, #setMessageProperty', () => {
+    it('should return expected response', () => {
+      const expectedValue = 'message';
+      service.setMessageProperty(expectedValue);
+      expect(service.getMessageProperty()).toEqual(expectedValue);
+    });
+  });
+  
+  describe('#clearMessageProperty', () => {
+    it('should clear property', () => {
+      const expectedValue = 'message';
+      service.setMessageProperty(expectedValue);
+      expect(service.getMessageProperty()).toEqual(expectedValue);
+      service.clearMessageProperty();
+      expect(service.getMessageProperty()).toEqual('');
+    });
+  });
+  
+  describe('#setupPageErrorMessageFromResponse', () => {
+    const parameters = [
+      {
+        description: 'should set property when error status is 400',
+        error: { status: 400 },
+        expectedValue: 'errMessage.http.badRequest'
+      },
+      {
+        description: 'should set property when error status is 401',
+        error: { status: 401 },
+        expectedValue: 'errMessage.http.unAuthorized'
+      },
+      {
+        description: 'should set property when error status is 404',
+        error: { status: 404 },
+        expectedValue: 'errMessage.http.notFound'
+      },
+      {
+        description: 'should set property when error status is 500 _ Duplicated key.',
+        error: { status: 500, error: { message: 'Duplicated key.' } },
+        expectedValue: 'errMessage.http.duplicateKeyException'
+      },
+      {
+        description: 'should set property when error status is 500 _ Exclusive error occurred.',
+        error: { status: 500, error: { message: 'Exclusive error occurred.' } },
+        expectedValue: 'errMessage.http.exclusiveProcessingException'
+      },
+      {
+        description: 'should set property when error status is 500 _ There is no stock.',
+        error: { status: 500, error: { message: 'There is no stock.' } },
+        expectedValue: 'errMessage.http.outOfStockException'
+      },
+      {
+        description: 'should set property when error status is 500 _ Data not found.',
+        error: { status: 500, error: { message: 'Data not found.' } },
+        expectedValue: 'errMessage.http.datNotFoundException'
+      },
+      {
+        description: 'should set property when error status is 500 _ Another message',
+        error: { status: 500, error: { message: 'Another message' } },
+        expectedValue: 'errMessage.http.internalServerError'
+      },
+      {
+        description: 'should set property when error status is 501',
+        error: { status: 501 },
+        expectedValue: 'errMessage.http.GenericError'
+      }
+    ];
+  
+    parameters.forEach((parameter) => {
+      it(parameter.description, () => {
+        service.setupPageErrorMessageFromResponse(parameter.error);
+        expect(service.getMessageProperty()).toEqual(parameter.expectedValue);
+      });
+    });
   });
 });
