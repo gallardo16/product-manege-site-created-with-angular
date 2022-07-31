67a21d92661a858131d16345ede7b97b66b33ea1
gallardo <aiki.hosogaya@gmail.com>
2022-07-31 18:44:07 +0900

    create loading test

--- product-manage-site-for-hands-on/src/app/core/components/loading/loading.component.spec.ts
+++ product-manage-site-for-hands-on/src/app/core/components/loading/loading.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { LoadingService } from '../../services/loading.service';
 import { LoadingComponent } from './loading.component';
 
-xdescribe('LoadingComponent', () => {
+describe('LoadingComponent', () => {
   let component: LoadingComponent;
   let fixture: ComponentFixture<LoadingComponent>;
+  let service: LoadingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,9 +17,26 @@ xdescribe('LoadingComponent', () => {
     fixture = TestBed.createComponent(LoadingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    service = TestBed.inject(LoadingService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('#constractor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#startLoading', () => {
+    it('should start loading', () => {
+      service.startLoading();
+      expect(service.isLoading).toBe(true);
+    });
+  });
+  
+  describe('#stopLoading', () => {
+    it('should stop loading', () => {
+      service.stopLoading();
+      expect(service.isLoading).toBe(false);
+    });
   });
 });
